8816e19462d2d38cb09c15427862b1a5efeabcfd
Sierra1393 <sotir693@gmail.com>
2026-08-18 18:53:01 +0200

    Restaurar src/components/UfDetailView.tsx sense text de diagnòstic

--- src/components/UfDetailView.tsx
+++ src/components/UfDetailView.tsx
@@ -1,6 +1,3 @@
-Exit code: 0
-Wall time: 1 seconds
-Output:
 import React, { useState } from 'react';
 import { FormativeUnit, Question, ExamAttempt, TopicContent } from '../types';
 import { QuizEngine } from './QuizEngine';
